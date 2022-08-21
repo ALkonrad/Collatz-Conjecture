d81cb49190d8ed6198ff28c9b92f213858b12f58
ALkonrad <kaunelundberg@gmail.com>
2022-08-21 10:44:35 +0200

    Making the program work

---
+++ collatzConjecture.py
@@ -0,0 +1,9 @@
+starting_number = 2 ** 1000
+
+while starting_number != 1:
+    if starting_number % 2 == 0:
+        starting_number /= 2
+    else:
+        starting_number = starting_number * 3 + 1
+
+print("The number you set abides the collatz conjecture")
