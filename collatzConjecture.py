5343150dee909cd7a30af4737ac1a6da0819cee8
Konrad Aune-Lundberg <111636830+ALkonrad@users.noreply.github.com>
2022-08-22 08:13:15 +0200

    Update collatzConjecture.py
    Changing line 5 division from /= to //= this results in only integer division that can support much larger numbers.

--- collatzConjecture.py
+++ collatzConjecture.py
@@ -2,7 +2,7 @@ starting_number = 2 ** 1000
 
 while starting_number != 1:
     if starting_number % 2 == 0:
-        starting_number /= 2
+        starting_number //= 2
     else:
         starting_number = starting_number * 3 + 1
 
